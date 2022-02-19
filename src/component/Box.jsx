455bf475e34070debd1cf96c82f6cadeef8fd626
Kiratikorn <bassball005@gmail.com>
2022-02-19 12:24:32 +0700

    test

--- src/component/Box.jsx
+++ src/component/Box.jsx
@@ -1,11 +1,15 @@
-import React from 'react';
+import React from "react";
 
-const Box = ({title,complete}) => {
-    return (
-        <div className= {`w-60 h-20 flex justify-center items-center rounded-md shadow-xl mb-10 mx-2 text-center transform hover:scale-x-110 hover:scale-y-110 transition duration 300 ${complete ? 'bg-green-300' : 'bg-red-300'}`}>
-            {title}
-        </div>
-    );
+const Box = ({ title, complete }) => {
+  return (
+    <div
+      className={`w-60 h-20 flex justify-center items-center mb-10 mx-2 rounded-md 
+        shadow-xl text-center transform hover:scale-110 transition duration 300 
+        ${complete ? "bg-green-300" : "bg-red-300"}`}
+    >
+      {title}
+    </div>
+  );
 };
 
 export default Box;
